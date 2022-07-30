330bbc401474788101ada8747ee31380ed7d917e
Robert Hardy <hardyrar@gmail.com>
2022-07-30 15:29:19 +0100

    Move entities to their destinations

--- Assets/Scripts/Systems/MoveToDestinationSystem.cs
+++ Assets/Scripts/Systems/MoveToDestinationSystem.cs
@@ -1,3 +1,4 @@
+using Components;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
@@ -8,28 +9,28 @@ namespace Systems
     {
         protected override void OnUpdate()
         {
-            // Assign values to local variables captured in your job here, so that it has
-            // everything it needs to do its work when it runs later.
-            // For example,
-            float deltaTime = Time.DeltaTime;
+            var deltaTime = Time.DeltaTime;
+            var up = new float3(0, 1, 0);
 
-            // This declares a new kind of job, which is a unit of work to do.
-            // The job is declared as an Entities.ForEach with the target components as parameters,
-            // meaning it will process all entities in the world that have both
-            // Translation and Rotation components. Change it to process the component
-            // types you want.
-        
-        
-        
-            Entities.ForEach((ref Translation translation, in Rotation rotation) => {
-                // Implement the work to perform for each entity here.
-                // You should only access data that is local or that is a
-                // field on this job. Note that the 'rotation' parameter is
-                // marked as 'in', which means it cannot be modified,
-                // but allows this job to run in parallel with other jobs
-                // that want to read Rotation component data.
-                // For example,
-                translation.Value += math.mul(rotation.Value, new float3(0, 0, 1)) * deltaTime;
+            Entities.ForEach((
+                ref Translation translation,
+                ref Rotation rotation,
+                in Destination destination,
+                in MovementSpeed movementSpeed) =>
+            {
+                // Return early if we're already at the destination
+                if (math.all(destination.value == translation.Value)) return;
+
+                var toDestination = destination.value - translation.Value;
+                rotation.Value = quaternion.LookRotation(toDestination, up);
+
+                /* Move towards destination with each frame. If movement goes beyond destination,
+                 then only move to the destination.*/
+                var movement = math.normalize(toDestination) * movementSpeed.value * deltaTime;
+                if (math.length(movement) >= math.length(toDestination))
+                    translation.Value = destination.value;
+                else
+                    translation.Value += movement;
             }).Schedule();
         }
     }
